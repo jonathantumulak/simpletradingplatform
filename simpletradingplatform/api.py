af3f03d30de47746b6a3252849479c43bed5155a
Jonathan Tumulak <jonathan@newlogic.com>
2024-06-09 23:56:42 +0800

    STP-1 Create api for stocks and orders

--- simpletradingplatform/api.py
+++ simpletradingplatform/api.py
@@ -1,4 +1,8 @@
 from rest_framework import routers
+from trading.views import (
+    OrderViewSet,
+    StockViewSet,
+)
 from users.views import UserViewSet
 
 
@@ -7,3 +11,5 @@ router = routers.DefaultRouter()
 router.trailing_slash = "/?"
 
 router.register(r"users", UserViewSet)
+router.register(r"stock", StockViewSet)
+router.register(r"orders", OrderViewSet)
--- trading/views.py
+++ trading/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from trading.models import (
+    Order,
+    Stock,
+)
+from trading.serializers import (
+    OrderSerializer,
+    StockSerializer,
+)
 
 
-# Create your views here.
+class StockViewSet(viewsets.ModelViewSet):
+    """View set for listing available stocks"""
+
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    http_method_names = [
+        "get",
+    ]
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """View set for orders"""
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
+    http_method_names = [
+        "get",
+        "post",
+    ]
+
+    def get_queryset(self, *args, **kwargs) -> QuerySet:
+        return self.queryset.filter(user=self.request.user)
